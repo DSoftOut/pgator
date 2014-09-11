3639c96d18b204ebda0f4ec35d52f886f2863c48
NCrashed <NCrashed@gmail.com>
2014-09-12 00:49:54 +0400

    Travis fixes

---
+++ .travis-json_rpc.sql
@@ -0,0 +1,18 @@
+CREATE TABLE json_rpc
+(
+  method text NOT NULL,
+  sql_queries text[] NOT NULL,
+  arg_nums integer[] NOT NULL,
+  set_username boolean NOT NULL,
+  need_cache boolean NOT NULL,
+  read_only boolean NOT NULL,
+  reset_caches text[],
+  reset_by text[],
+  commentary text,
+  CONSTRAINT json_rpc_pkey PRIMARY KEY (method)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE json_rpc
+  OWNER TO postgres;
