4ad8617925bee192105d895f680b0387a8e0dd02
Denis Feklushkin <denis.feklushkin@gmail.com>
2014-10-28 02:00:52 +0700

    owners fixed

--- Debian/build.sh
+++ Debian/build.sh
@@ -3,9 +3,10 @@
 set -e
 
 WORKDIR=/tmp/build
-DEBDIR=${WORKDIR}/DEBIAN
 
-mkdir -p ${DEBDIR}
+mkdir -p ${WORKDIR}
+
+cp -r Debian/DEBIAN ${WORKDIR}/
 
 VERSION=`git describe --match=v* | sed 's/^v//'`
 
@@ -15,18 +16,14 @@ Version: $VERSION
 Depends: libpq5 (>= 9.4~)
 Maintainer: DSoftOut Crew
 Description: Server that transforms JSON-RPC calls into SQL queries for PostgreSQL
-" > ${DEBDIR}/control
-
-cp Debian/conffiles ${DEBDIR}
+" > ${WORKDIR}/DEBIAN/control
 
 mkdir -p ${WORKDIR}/etc/
 mkdir -p ${WORKDIR}/usr/bin/
 
 cp Debian/pgator.conf ${WORKDIR}/etc/
-chmod 644 ${WORKDIR}/etc/pgator.conf # maybe it is need 600 ?
-
 cp bin/pgator ${WORKDIR}/usr/bin/
 
-dpkg -b ${WORKDIR} pgator_${VERSION}.deb
+fakeroot dpkg-deb --build ${WORKDIR} pgator_${VERSION}.deb
 
-rm -rf ${WORKDIR}
+#rm -rf ${WORKDIR}
