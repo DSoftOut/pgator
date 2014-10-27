bc4d35e05fe41cd71278bb76574124d453fef2ee
Denis Feklushkin <denis.feklushkin@gmail.com>
2014-10-28 00:15:03 +0700

    deb package building

---
+++ Docker/build_env/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+WORKDIR=/tmp/build
+DEBDIR=${WORKDIR}/DEBIAN
+
+mkdir -p ${DEBDIR}
+
+VERSION=`git describe --match=v* | sed 's/^v//'`
+
+echo "Package: pgator
+Architecture: amd64
+Version: $VERSION
+Depends: libpq5 (>= 9.4)
+Maintainer: DSoftOut Crew
+Description: Server that transforms JSON-RPC calls into SQL queries for PostgreSQL
+" > ${DEBDIR}/control
+
+#to delete
+dpkg -b ${WORKDIR} pgator_${VERSION}.deb
+
+rm -rf ${DEBDIR}
