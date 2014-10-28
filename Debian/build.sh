18be08e3df8bed3c8225d213c01563785b5e1958
Denis Feklushkin <denis.feklushkin@gmail.com>
2014-10-28 12:44:05 +0700

    supervisor ini file added

--- Debian/build.sh
+++ Debian/build.sh
@@ -7,21 +7,20 @@ WORKDIR=/tmp/build
 mkdir -p ${WORKDIR}
 
 cp -r Debian/DEBIAN ${WORKDIR}/
+cp -r Debian/etc ${WORKDIR}/
 
 VERSION=`git describe --match=v* | sed 's/^v//'`
 
 echo "Package: pgator
 Architecture: amd64
 Version: $VERSION
-Depends: libpq5 (>= 9.4~)
+Depends: libpq5 (>= 9.4~), supervisor (>=3.0~)
 Maintainer: DSoftOut Crew
 Description: Server that transforms JSON-RPC calls into SQL queries for PostgreSQL
 " > ${WORKDIR}/DEBIAN/control
 
-mkdir -p ${WORKDIR}/etc/
 mkdir -p ${WORKDIR}/usr/bin/
 
-cp Debian/pgator.conf ${WORKDIR}/etc/
 cp bin/pgator ${WORKDIR}/usr/bin/
 
 fakeroot dpkg-deb --build ${WORKDIR} pgator_${VERSION}.deb
