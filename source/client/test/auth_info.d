664102213eb0309a5c8bd530ac35bf42c1d0e884
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-12-29 14:55:27 +0700

    cleanup

--- source/client/test/auth_info.d
+++ source/client/test/auth_info.d
@@ -14,7 +14,6 @@ class AuthInfoTestCase : ITestCase
                 true
             )
         );
-        //insertRow(pool, tableName, JsonRpcRow("without_auth", 0, "SELECT $1::int8 + $2::int8 as test_field;"));
     }
     
     /**
@@ -23,7 +22,6 @@ class AuthInfoTestCase : ITestCase
     protected void deleteMethods(shared IConnectionPool pool, string tableName)
     {
         removeRow(pool, tableName, "with_auth");
-        //removeRow(pool, tableName, "without_auth");
     }
     
     /**
@@ -34,8 +32,5 @@ class AuthInfoTestCase : ITestCase
     {
         auto with_auth = api.runRpc!"with_auth".assertOk!(Column!(string, "user_pass"));
         assert(with_auth.user_pass[0] == "Aladdinopen sesame");
-        
-        //auto result2 = api.runRpc!"without_auth"(2, 1).assertOk!(Column!(ulong, "test_field1"), Column!(ulong, "test_field2"));
-        //assert(result2.test_field1[0] == 3 && result2.test_field2[0] == 1);
     }
 }
