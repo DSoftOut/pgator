43bac5d106682c8d86800387787d6df0b591e236
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-28 05:16:14 +0700

    backup (tests unfinished)

--- .test.sh
+++ .test.sh
@@ -4,7 +4,12 @@ set -ve
 CONNINFO=`jq '.sqlServer.connString' ${1}`
 UNQUOTED=`echo $CONNINFO | xargs`
 
-dub build --build=unittest
-psql -f .test_pgator_rpc_table.sql "$UNQUOTED"
-./pgator --config=${1} --debug=true --test=true || true
-./pgator --config=${1} --debug=true
+#~ dub build --build=unittest
+#~ psql -f .test_pgator_rpc_table.sql "$UNQUOTED"
+#~ ./pgator --config=${1} --debug=true --test=true || true
+#~ ./pgator --config=${1} --debug=true
+
+ADDRESS=`jq '.listenAddresses[0]' ${1}`
+PORT=`jq '.listenPort' ${1}`
+
+dub run pgator:test --build=unittest-cov -- "$ADDRESS" "$PORT"
--- source/test/package.d
+++ source/test/package.d
@@ -3,16 +3,32 @@ module pgator.test;
 
 version(IntegrationTest)
 {
+    import std.conv;
+    import std.net.curl;
 
-import std.conv;
-import std.net.curl;
+    void main(string[] args)
+    {
+        string httpHost = args[0];
+        string port = args[1];
 
-void main(string[] args)
-{
-    string httpHost = args[0];
-    string port = args[1];
+        HTTP http = HTTP("test client");
 
-    auto content = post(httpHost~":"~port~"/", [1,2,3,4]);
-}
+        http.url = "http://"~httpHost~":"~port~"/";
+        http.method = HTTP.Method.post;
+    }
 
+    struct QueryAnswer
+    {
+        string query;
+        string answer;
+        ushort httpCode = 200;
+        string contentType = "application/json";
+    }
+
+    alias QA = QueryAnswer;
+
+    QueryAnswer[] tests = [
+        QA("asd", "qwe"),
+        QA("asd", "qwe", "asd"),
+    ];
 }
