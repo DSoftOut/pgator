c195b9ef7041ea544e20a5d2ba263e9f4335573e
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-28 18:15:54 +0700

    expected value check

--- source/test/package.d
+++ source/test/package.d
@@ -21,13 +21,24 @@ version(IntegrationTest)
             try
             {
                 auto p = post(url, t.query, http);
+
+                import vibe.data.json;
+                Json result = parseJsonString(p.to!string);
+                Json expected = parseJsonString(t.expectedAnswer);
+
+                enforce(result == expected, "result: "~result.toString~", expected: "~expected.toString);
             }
             catch(CurlException e)
             {
                 import std.algorithm.searching;
 
                 if(e.msg.canFind("status code 400 (Bad Request)") && t.httpCode != 400)
-                    throw new Exception("Test line "~t.line.to!string~": "~e.msg, __FILE__, __LINE__);
+                    throw new Exception(e.msg, __FILE__, __LINE__);
+            }
+            catch(Exception e)
+            {
+                e.msg = "Test line "~t.line.to!string~": "~e.msg;
+                throw e;
             }
         }
     }
@@ -36,7 +47,7 @@ version(IntegrationTest)
     {
         size_t line;
         string query;
-        string answer;
+        string expectedAnswer;
         ushort httpCode = 200;
         string contentType = "application/json";
     }
@@ -54,7 +65,12 @@ q"EOS
 }
 EOS",
 
-"qwe"
+q"EOS
+{
+    "params": [ 123 ],
+    "id": 1
+}
+EOS"
         ),
         QA(__LINE__, "asd", "qwe", 211),
     ];
