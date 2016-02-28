93cc30b5afef5d0c5d31bd3583c353ed6e659bc8
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-28 16:33:04 +0700

    backup

--- .test_pgator_rpc_table.sql
+++ .test_pgator_rpc_table.sql
@@ -5,15 +5,27 @@ CREATE TABLE pgator_rpc
   method text NOT NULL,
   sql_query text NOT NULL,
   args text[] NOT NULL,
+  one_row_flag boolean,
   --set_username boolean NOT NULL,
   --read_only boolean NOT NULL,
   --commentary text,
-  one_row_flag boolean,
 
   CONSTRAINT pgator_rpc_pkey PRIMARY KEY (method)
 );
 
+CREATE OR REPLACE FUNCTION show_error(message text, internal boolean default false, error_code text default 'P0001'::text)
+RTURNS void LANGUAGE plpgsql AS $_$
+begin
+        if internal then
+                raise '%', message using errcode = error_code;
+        else
+                raise 'CAUGHT_ERROR: %', message using errcode = error_code;
+        end if;
+end;
+$_$;
+
 INSERT INTO pgator_rpc VALUES
 ('echo', 'SELECT $1::text as echoed', '{"value_for_echo"}', false),
 ('echo2', 'SELECT $1::text', '{"value_for_echo"}', NULL),
-('wrong_sql_statement', 'wrong SQL statement', '{}', false);
+('wrong_sql_statement', 'wrong SQL statement', '{}', false),
+('show_error', 'SELECT show_error($1, $2, $3)', '{"msg", "internalFlag", "errorCode"}');
--- source/test/package.d
+++ source/test/package.d
@@ -15,10 +15,12 @@ version(IntegrationTest)
 
         http.url = "http://"~httpHost~":"~port~"/";
         http.method = HTTP.Method.post;
+        http.addRequestHeader("Content-Type", "application/json");
     }
 
     struct QueryAnswer
     {
+        size_t line;
         string query;
         string answer;
         ushort httpCode = 200;
@@ -28,7 +30,23 @@ version(IntegrationTest)
     alias QA = QueryAnswer;
 
     QueryAnswer[] tests = [
-        QA("asd", "qwe"),
-        QA("asd", "qwe", "asd"),
+        QA(__LINE__,
+q"EOS
+{
+    "jsonrpc": "2.0",
+    "method": "echo",
+    "params": [ 123 ],
+    "id": 1
+}
+EOS",
+
+"qwe"
+        ),
+        QA(__LINE__, "asd", "qwe", 211),
     ];
+
+    foreach(t; tests)
+    {
+        http.
+    }
 }
