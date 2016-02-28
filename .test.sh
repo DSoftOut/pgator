078b3dbda686ffc7cc747494b8d029e8319f8d94
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-28 19:43:23 +0700

    test script fixed

--- .test.sh
+++ .test.sh
@@ -6,13 +6,17 @@ CONNINFO_UNQUOTED=`echo $CONNINFO | xargs`
 
 dub build --build=unittest
 psql -f .test_pgator_rpc_table.sql "$CONNINFO_UNQUOTED"
-./pgator --config=${1} --debug=true --test=true || true #TODO: check exitcode==1
+
+# Test calls table by preparing statements
+`./pgator --config=${1} --debug=true --test=true; if [ $? -ne 1 ]; then exit 1; fi` # Some statements should be bad
+
+# Start pgator server
 ./pgator --config=${1} --debug=true &
+trap "kill %%" EXIT
 
 ADDRESS=`jq '.listenAddresses[0]' ${1}`
 ADDRESS_UNQUOTED=`echo $ADDRESS | xargs`
 PORT=`jq '.listenPort' ${1}`
 
-dub run pgator:test --build=unittest-cov -- "$ADDRESS_UNQUOTED" "$PORT" || true
-
-kill %%
+# Start test client
+dub run pgator:test --build=unittest -- "$ADDRESS_UNQUOTED" "$PORT"
