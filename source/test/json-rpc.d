acd2c0da58256f7057bd7e412ff7facace9052e6
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-09-22 01:56:23 +0700

    vibe REST test moved to separate file

--- source/test/json-rpc.d
+++ source/test/json-rpc.d
@@ -1,5 +1,5 @@
 // integration tests
-module pgator.test;
+module pgator.test.json_rpc;
 
 version(IntegrationTest)
 {
@@ -58,7 +58,11 @@ version(IntegrationTest)
             }
         }
 
-        vibedRESTEmulationTests(httpUrl);
+        {
+            import pgator.test.vibe_rest;
+
+            vibedRESTEmulationTests(httpUrl);
+        }
     }
 
     struct QueryAnswer
@@ -671,27 +675,4 @@ EOS"
 ),
 
 ];
-}
-
-version(IntegrationTest)
-void vibedRESTEmulationTests(string httpUrl)
-{
-    import vibe.web.rest;
-
-    interface ITest
-    {
-        string getEchoText(string value_for_echo);
-        long getEchoBigint(long value_for_echo);
-        double getEchoFloat8(double value_for_echo);
-        double postEchoFloat8(double value_for_echo);
-    }
-
-    auto m = new RestInterfaceClient!ITest(httpUrl);
-
-    assert(m.getEchoText("abc") == "abc");
-    assert(m.getEchoBigint(123456) == 123456);
-    assert(m.getEchoFloat8(123.45) == 123.45);
-
-    //assert(m.getEchoBigint(123.456) == 123.456); //TODO: causes error, need to check this case
-    //assert(m.postEchoFloat8(123.45) == 123.45); //TODO: POST support
 }
---
+++ source/test/vibe_rest.d
@@ -0,0 +1,24 @@
+module pgator.test.vibe_rest;
+
+version(IntegrationTest)
+void vibedRESTEmulationTests(string httpUrl)
+{
+    import vibe.web.rest;
+
+    interface ITest
+    {
+        string getEchoText(string value_for_echo);
+        long getEchoBigint(long value_for_echo);
+        double getEchoFloat8(double value_for_echo);
+        double postEchoFloat8(double value_for_echo);
+    }
+
+    auto m = new RestInterfaceClient!ITest(httpUrl);
+
+    assert(m.getEchoText("abc") == "abc");
+    assert(m.getEchoBigint(123456) == 123456);
+    assert(m.getEchoFloat8(123.45) == 123.45);
+
+    //assert(m.getEchoBigint(123.456) == 123.456); //TODO: causes error, need to check this case
+    //assert(m.postEchoFloat8(123.45) == 123.45); //TODO: POST support
+}
